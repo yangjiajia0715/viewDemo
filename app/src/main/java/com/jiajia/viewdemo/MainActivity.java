be44dda82a02f62074e1d052419e061b64d72b10
杨佳佳 <dccb-wbyangjj@sdc.icbc.com.cn>
2019-05-30 14:03:08 +0800

    xmlpullParse

--- app/src/main/java/com/jiajia/viewdemo/MainActivity.java
+++ app/src/main/java/com/jiajia/viewdemo/MainActivity.java
@@ -1,30 +1,32 @@
 package com.jiajia.viewdemo;
 
+import android.content.res.XmlResourceParser;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+/**
+ * @author yangjiajia
+ */
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        try {
-            XmlPullParserFactory xmlPullParserFactory = XmlPullParserFactory.newInstance();
-            XmlPullParser xmlPullParser = xmlPullParserFactory.newPullParser();
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
-        }
-
+        LayoutInflater.from(this).inflate(R.layout.item_temp, null);
     }
 
     @OnClick({R.id.btn_view_1, R.id.btn_view_2, R.id.btn_view_3, R.id.btn_view_4,
@@ -32,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn_view_1:
+                xmlPullParser();
                 break;
             case R.id.btn_view_2:
                 break;
@@ -47,6 +50,42 @@ public class MainActivity extends AppCompatActivity {
                 break;
             default:
                 break;
+        }
+    }
+
+    private void xmlPullParser() {
+        //                try {
+//                    XmlPullParserFactory xmlPullParserFactory = XmlPullParserFactory.newInstance();
+//                    XmlPullParser xmlPullParser = xmlPullParserFactory.newPullParser();
+//                } catch (XmlPullParserException e) {
+//                    e.printStackTrace();
+//                }
+
+        XmlResourceParser parser = getResources().getXml(R.xml.file_paths);
+        try {
+            int eventType;
+            while ((eventType = parser.next()) != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_DOCUMENT) {
+                    Log.d(TAG, "xmlPullParser: Start document");
+                } else if (eventType == XmlPullParser.START_TAG) {
+                    Log.d(TAG, "xmlPullParser: START_TAG " + parser.getName()
+                            + ",getAttributeCount=" + parser.getAttributeCount());
+                    if (parser.getName().endsWith("-path")) {
+                        Log.w(TAG, "xmlPullParser: START_TAG " + parser.getName()
+                                + ",getAttributeCount=" + parser.getAttributeCount()
+                                + parser.getAttributeName(0)
+                                + ":" + parser.getAttributeValue(0));
+                    }
+                } else if (eventType == XmlPullParser.END_TAG) {
+                    Log.d(TAG, "xmlPullParser: END_TAG " + parser.getName());
+                } else if (eventType == XmlPullParser.TEXT) {
+                    Log.d(TAG, "xmlPullParser: TEXT" + parser.getText());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
         }
     }
 }
